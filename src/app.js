ad0778d0b2c1634bc5fdc5037ca3ebda4c00e60f
tulzlol <tulo.nv@gmail.com>
2023-09-27 22:10:13 -0300

    entrega-4

--- src/app.js
+++ src/app.js
@@ -1,86 +1,28 @@
 import express from "express";
-import { manager } from "./ProductManager.js";
+import productsRouter from "./routes/products.router.js";
+import cartRouter from "./routes/carts.router.js";
+// import { engine } from "express-handlebars";
+import { __dirname } from "./utils.js";
 
 const app = express();
 
 app.use(express.json());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(__dirname + "/public"));
 
-app.get("/api/products", async (req, res) => {
-    try {
-        const products = await manager.getProducts();
-        res.status(200).json({ message: "Products found", products });
-    } catch (error) {
-        res.status(500).json({ message: error.message });
-    }
-});
 
-app.get("/api/products/:id", async (req, res) => {
-    const { id } = req.params;
-    console.log(id);
-    try {
-        const product = await manager.getProductById(+id);
-        console.log(product);
-        if (!product) {
-            return res
-                .status(404)
-                .json({ message: "Product not found with the id provided" });
-        }
-        res.status(200).json({ message: "Product found", product });
-    } catch (error) {
-        res.status(500).json({ message: error.message });
-    }
-});
 
-app.post("/api/products", async (req, res) => {
-    const { title, description, price, thumbnail, code, stock } = req.body;
-    if (!title || !description || !price || !thumbnail || !code || !stock) {
-        return res.status(400).json({ message: "Some data is missing" });
-    }
-    try {
-        const response = await manager.addProduct(
-            title,
-            description,
-            price,
-            thumbnail,
-            code,
-            stock
-        );
-        res.status(200).json({ message: "Product created", product: response });
-    } catch (error) {
-        res.status(500).json({ message: error.message });
-    }
-});
+// Handlebars
+// app.engine("handlebars", engine());
+// app.set("view",__dirname+"/views");
+// app.set("view engine", "handlebars");
 
-app.delete("/api/products/:idProduct", async (req, res) => {
-    const { idProduct } = req.params;
-    try {
-        const response = await manager.deleteProduct(+idProduct);
-        if (!response) {
-            return res
-                .status(404)
-                .json({ message: "Product not found with the id provided" });
-        }
-        res.status(200).json({ message: "Product deleted" });
-    } catch (error) {
-        res.status(500).json({ message: error.message });
-    }
-});
+// routes
 
-app.put("/api/products/:idProduct", async (req, res) => {
-    const { idProduct } = req.params;
-    try {
-        const response = await manager.updateProduct(+idProduct, req.body);
-        if (!response) {
-            return res
-                .status(404)
-                .json({ message: "Product not found with the id provided" });
-        }
-        res.status(200).json({ message: "Product updated" });
-    } catch (error) {
-        res.status(500).json({ message: error.message });
-    }
-});
+app.use("/api/products", productsRouter);
+app.use("/api/carts", cartRouter);
 
 app.listen(8080, () => {
     console.log("Listening on port 8080");
-});
+})
