6e715cdcba2712d53bb947282c8b261161d50e45
tulzlol <tulo.nv@gmail.com>
2023-10-05 17:04:16 -0300

    port-change

--- src/app.js
+++ src/app.js
@@ -28,7 +28,7 @@ app.use("/api/carts", cartRouter);
 
 
 const httpServer = app.listen(port, () => {
-    console.log("Listening on port 3000");
+    console.log("Listening on port 8080");
 });
 
 const socketServer = new Server(httpServer);
